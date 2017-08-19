4a2ccd9727d2082050420f6453724e52d8bbd9a7
xavier dupré <xavier.dupre@gmail.com>
2017-08-19 15:31:04 +0200

    split circleci commands

--- doc/conf.py
+++ doc/conf.py
@@ -3,7 +3,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from td1a_unit_test_ci import __version__
 
 extensions = ['sphinx.ext.autodoc',
