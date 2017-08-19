3937d0e696cf306589cc96a59a4f8cf8858a9f49
xavier dupré <xavier.dupre@gmail.com>
2017-08-19 19:51:45 +0200

    applies pep257

--- td1a_unit_test_ci/polynom.py
+++ td1a_unit_test_ci/polynom.py
@@ -1,16 +1,14 @@
-"""
-Functions about polynoms.
-"""
+"""Functions about polynoms."""
 
 
 def solve_polynom(a, b, c):
-    """
-    Solve a real polynom of degree 2.
+    """Solve a real polynom of degree 2.
 
     :param a: coefficient for :math:`x^2`
     :param b: coefficient for :math:`x`
     :param c: coefficient for :math:`1`
     :return: couple of solutions or one if the degree is 1.
+
     """
     if a == 0:
         # One degree.
