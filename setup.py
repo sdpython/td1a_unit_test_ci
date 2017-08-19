8347a7c247e05050999f4d67aa98856657412a1d
xavier dupré <xavier.dupre@gmail.com>
2017-08-19 14:02:28 +0200

    move source for the setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='td1a_unit_test_ci',
+      version='0.1',
+      description="module très simple pour essayer l'inégration continue",
+      author='Xavier Dupré',
+      author_email='xavier.dupre@gmail.com',
+      url='https://github.com/sdpython/td1a_unit_test_ci',
+      packages=find_packages(),
+     )
