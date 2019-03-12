747a2839184aa6a58b16b933189cad30af930214
xavier dupré <xavier.dupre@gmail.com>
2019-03-12 15:46:49 +0100

    setup requirements

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ here = os.path.dirname(__file__)
 packages = find_packages(where=here)
 package_dir = {k: os.path.join(here, k.replace(".", "/")) for k in packages}
 
+with open(os.path.join(here, "requirements.txt"), "r") as f:
+    requirements = f.read().split("\n")
 
 setup(name='td1a_unit_test_ci',
       version='0.1',
@@ -16,4 +18,7 @@ setup(name='td1a_unit_test_ci',
       author_email='xavier.dupre@gmail.com',
       url='https://github.com/sdpython/td1a_unit_test_ci',
       packages=packages,
-      package_dir=package_dir)
+      package_dir=package_dir,
+      # requires indique quels packages doivent être installés
+      # également pour que cela fonctionne
+      requires=requirements)
